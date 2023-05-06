67418b64f22f5c7c40cd05830aeb383f57364e06
piero <piero.guerrero@utec.edu.pe>
2023-05-06 14:45:04 -0500

    server.py renamed to app.py

--- app.py
+++ app.py
@@ -15,8 +15,10 @@ db = SQLAlchemy(app)
 class genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     genre_name = db.Column(db.String(20), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False,
+                           server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
+                            server_default=db.text("now()"))
     games_of_genre = db.relationship('game', backref='genre', lazy=True)
 
     def __init__(self, id, name):
@@ -35,11 +37,14 @@ class genre(db.Model):
             'modified_at': self.modified_at,
         }
 
+
 class platform(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     platform_name = db.Column(db.String(10), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False,
+                           server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
+                            server_default=db.text("now()"))
 
     def __init__(self, id, name):
         self.id = id
@@ -65,7 +70,8 @@ class game(db.Model):
     synopsis = db.Column(db.String(1000), nullable=True)
     image = db.Column(db.String(500), nullable=True)
     compras = db.relationship('Compra', backref='game', lazy=True)
-    gamepublishers = db.relationship('Game_publisher', backref='ggame_publisher', lazy=True)
+    gamepublishers = db.relationship('Game_publisher',
+                                     backref='ggame_publisher', lazy=True)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False,
                            server_default=db.text("now()"))
     modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
@@ -92,28 +98,33 @@ class game(db.Model):
             'created_at': self.created_at,
             'modified_at': self.modified_at,
         }
-    
+
+
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
+    id = db.Column(db.String(36), primary_key=True,
+                   default=lambda: str(uuid.uuid4()),
+                   server_default=db.text("uuid_generate_v4()"))
     firstname = db.Column(db.String(80), nullable=False)
     lastname = db.Column(db.String(120), unique=False, nullable=False)
-    email = db.Column(db.String(300), unique=True , nullable=False)
-    bio = db.Column(db.String(500) , nullable=False )
+    email = db.Column(db.String(300), unique=True, nullable=False)
+    bio = db.Column(db.String(500), nullable=False)
     image = db.Column(db.String(500), nullable=True)
     compras = db.relationship('Compra', backref='usuario', lazy=True)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False,
+                           server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
+                            server_default=db.text("now()"))
 
-    def __init__(self , firstname, lastname , email , bio ):
+    def __init__(self, firstname, lastname, email, bio):
         self.firstname = firstname
         self.lastname = lastname
-        self.email = email 
-        self.bio = bio 
+        self.email = email
+        self.bio = bio
         self.created_at = datetime.utcnow()
 
     def __repr__(self):
-        return '<Usuario %r>' % (self.firstname, self.lastname)
+        return '<Usuario %r %r>' % (self.firstname, self.lastname)
 
     def serialize(self):
         return {
@@ -125,23 +136,29 @@ class Usuario(db.Model):
             'image': self.image,
             'created_at': self.created_at,
             'modified_at': self.modified_at,
-        } 
+        }
+
 
 class Compra(db.Model):
     __tablename__ = 'compras'
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
-    usuario_id = db.Column(db.String(36), db.ForeignKey('usuarios.id'), nullable=True)
+    id = db.Column(db.String(36), primary_key=True,
+                   default=lambda: str(uuid.uuid4()),
+                   server_default=db.text("uuid_generate_v4()"))
+    usuario_id = db.Column(db.String(36), db.ForeignKey('usuarios.id'),
+                           nullable=True)
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=True)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False,
+                           server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
+                            server_default=db.text("now()"))
 
-    def __init__(self ,usuario_id , game_id ):
+    def __init__(self, usuario_id, game_id):
         self.usuario_id
         self.game_id
         self.created_at = datetime.utcnow()
 
     def __repr__(self):
-        return '<Compra %r>' % ()
+        return '<Compra %r %r>' % (self.usuario_id, self.game_id)
 
     def serialize(self):
         return {
@@ -149,17 +166,22 @@ class Compra(db.Model):
             'usuario_id': self.usuario_id,
             'game_id': self.game_id,
             'created_at': self.created_at,
-            'modified_at': self.modified_at,    
-        }      
+            'modified_at': self.modified_at,
+        }
+
+
 class Publisher(db.Model):
     __tablename__ = 'publishers'
     id = db.Column(db.Integer, primary_key=True)
-    publisher_name = db.Column(db.String(200), unique=True , nullable=False)
-    gamepublishers = db.relationship('Game_publisher', backref='pgame_publisher', lazy=True)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
+    publisher_name = db.Column(db.String(200), unique=True, nullable=False)
+    gamepublishers = db.relationship('Game_publisher',
+                                     backref='pgame_publisher', lazy=True)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False,
+                           server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
+                            server_default=db.text("now()"))
 
-    def __init__(self, id, publisher_name ):
+    def __init__(self, id, publisher_name):
         self.id = id
         self.publisher_name = publisher_name
         self.created_at = datetime.utcnow()
@@ -172,21 +194,27 @@ class Publisher(db.Model):
             'id': self.id,
             'publisher_name': self.publisher_name,
             'created_at': self.created_at,
-            'modified_at': self.modified_at,    
-        } 
+            'modified_at': self.modified_at,
+        }
+
+
 class Game_publisher(db.Model):
     __tablename__ = 'game_publishers'
     id = db.Column(db.Integer, primary_key=True)
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=True)
-    publisher_id = db.Column(db.Integer, db.ForeignKey('publishers.id'), nullable=True)
-    gameplatforms = db.relationship('Game_platform', backref='game_publisher', lazy=True)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
+    publisher_id = db.Column(db.Integer, db.ForeignKey('publishers.id'),
+                             nullable=True)
+    gameplatforms = db.relationship('Game_platform', backref='game_publisher',
+                                    lazy=True)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False,
+                           server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
+                            server_default=db.text("now()"))
 
     def __init__(self, id, game_id, publisher_id):
         self.id = id
-        self.game_id = game_id  
-        self.publisher_id = publisher_id 
+        self.game_id = game_id
+        self.publisher_id = publisher_id
         self.created_at = datetime.utcnow()
 
     def __repr__(self):
@@ -198,22 +226,28 @@ class Game_publisher(db.Model):
             'game_id': self.game_id,
             'publisher_id': self.publisher_id,
             'created_at': self.created_at,
-            'modified_at': self.modified_at,    
-        } 
+            'modified_at': self.modified_at,
+        }
+
 
 class Game_platform (db.Model):
     __tablename__ = 'game_platforms'
     id = db.Column(db.Integer, primary_key=True)
-    game_publisher_id = db.Column(db.Integer, db.ForeignKey('game_publishers.id'), nullable=True)
-    platform_id = db.Column(db.Integer, db.ForeignKey('platform.id'), nullable=True)
+    game_publisher_id = db.Column(db.Integer,
+                                  db.ForeignKey('game_publishers.id'),
+                                  nullable=True)
+    platform_id = db.Column(db.Integer, db.ForeignKey('platform.id'),
+                            nullable=True)
     realese_year = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False,
+                           server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
+                            server_default=db.text("now()"))
 
-    def __init__(self, id ,game_publisher_id , platform_id ,realese_year ):
+    def __init__(self, id, game_publisher_id, platform_id, realese_year):
         self.id = id
-        self.game_publisher_id = game_publisher_id  
-        self.platform_id = platform_id 
+        self.game_publisher_id = game_publisher_id
+        self.platform_id = platform_id
         self.realese_year = realese_year
         self.created_at = datetime.utcnow()
 
@@ -227,14 +261,12 @@ class Game_platform (db.Model):
             'platform_id': self.platform_id,
             'realese_year': self.realese_year,
             'created_at': self.created_at,
-            'modified_at': self.modified_at,    
-        } 
-    
-     
+            'modified_at': self.modified_at,
+        }
 
 # Creates models
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#    db.create_all()
 
 
 # Start the server
