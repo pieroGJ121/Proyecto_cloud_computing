1ee265d54b9a72d5a2ae331b84a056a40e5e2451
Fabrizzio <fabrizzio.vilchez.e@utec.edu.pe>
2023-05-06 19:52:27 -0500

    New resources for html pages and new designs

---
+++ static/js/login/funciones_recover.js
@@ -0,0 +1,98 @@
+const pendingForms = new WeakMap()
+const data_recover = document.getElementById('data_recover')
+data_recover.addEventListener('submit', verify_submission)
+
+function verify_submission(e){
+    e.preventDefault()
+    e.stopPropagation()
+  
+    //Handle abort previous request
+    const formUserData = e.currentTarget
+    const previousController = pendingForms.get(formUserData)
+    if (previousController) {
+      previousController.abort()
+    }
+  
+    const controller = new AbortController()
+    pendingForms.set(formUserData, controller)
+    console.log('formCreateEmployee: ', formUserData)
+  
+    const formData = new FormData(formUserData)
+
+    fetch('/data_recover', {
+        method: 'POST',
+        body: formData,
+    }).then(function (response) {
+        console.log('response', response)
+        return response.json()
+      })
+      .then(function (responseJson) {
+        const message_error = document.getElementById('message_error')
+        const password_changer = document.getElementById('password_changer')
+        const submit1 = document.getElementById('submit1')
+        const email = document.getElementById('email');
+        const name = document.getElementById('name');
+        const text_email = document.getElementById('text_email');
+        const text_name = document.getElementById('text_name');
+        const text_change = document.getElementById('text_change');
+
+        if (!responseJson.success){
+          message_error.style.display = 'block'
+          password_changer.style.display = 'none'
+          message_error.innerHTML = responseJson.message
+          text_change.style.display = 'none'
+        } 
+        else{
+            message_error.style.display = 'none'
+            password_changer.style.display = 'block'
+            submit1.style.display = 'none'
+            text_change.style.display = 'block'
+            email.disabled = true;
+            name.disabled = true;
+            text_email.innerHTML = ''
+            text_name.innerHTML = ''
+        }
+      })
+}
+
+const pendingForms2 = new WeakMap()
+const password_changer = document.getElementById('password_changer')
+password_changer.addEventListener('submit', reset_password)
+
+function reset_password(e){
+    e.preventDefault()
+    e.stopPropagation()
+  
+    //Handle abort previous request
+    const formPassword = e.currentTarget
+    const previousController = pendingForms2.get(formPassword)
+    if (previousController) {
+      previousController.abort()
+    }
+  
+    const controller = new AbortController()
+    pendingForms2.set(formPassword, controller)
+    console.log('formCreateEmployee: ', formPassword)
+  
+    const formData = new FormData(formPassword)
+
+    fetch('/reset_password', {
+        method: 'POST',
+        body: formData,
+    }).then(function (response) {
+        console.log('response', response)
+        return response.json()
+      })
+      .then(function (responseJson) {
+        const message_error2 = document.getElementById('message_error2')
+
+        if (!responseJson.success){
+          message_error2.style.display = 'block'
+          message_error2.innerHTML = responseJson.message
+        } 
+        else{
+            message_error2.style.display = 'none'
+            window.location.href = "/login";
+        }
+      })
+}
---
+++ static/js/login/funciones_register.js
@@ -0,0 +1,61 @@
+const MAX_WORDS = 50; // Define el número máximo de palabras permitido
+const textarea = document.querySelector('textarea');
+const pendingForms = new WeakMap()
+const register_data = document.getElementById('register_data')
+register_data.addEventListener('submit', register)
+
+function register(e){
+    e.preventDefault()
+    e.stopPropagation()
+  
+    //Handle abort previous request
+    const formUserData = e.currentTarget
+    const previousController = pendingForms.get(formUserData)
+    if (previousController) {
+      previousController.abort()
+    }
+  
+    const controller = new AbortController()
+    pendingForms.set(formUserData, controller)
+    console.log('formCreateEmployee: ', formUserData)
+  
+    const formData = new FormData(formUserData)
+
+    fetch('/create_user', {
+        method: 'POST',
+        body: formData,
+    }).then(function (response) {
+        console.log('response', response)
+        return response.json()
+      })
+      .then(function (responseJson) {
+        const message_error = document.getElementById('message_error')
+
+        if (!responseJson.success){
+          message_error.style.display = 'block'
+          message_error.innerHTML = responseJson.message
+        } 
+        else{
+          message_error.style.display = 'none'
+          window.location.href = "/";
+        }
+      })
+}
+
+var autosizeScript = document.createElement('script');
+autosizeScript.src = "https://cdnjs.cloudflare.com/ajax/libs/autosize.js/4.0.2/autosize.min.js";
+document.head.appendChild(autosizeScript);
+
+// Espera a que la biblioteca Autosize se cargue y, a continuación, inicializa el texto
+autosizeScript.onload = function() {
+  autosize(document.querySelector('textarea'));
+}
+
+// Agrega un listener para el evento input en el textarea
+textarea.addEventListener('input', () => {
+  const words = textarea.value.trim().split(/\s+/); // Divide el contenido del textarea en palabras
+  if (words.length > MAX_WORDS) {
+    textarea.value = words.slice(0, MAX_WORDS).join(' '); // Si se supera el límite de palabras, se borra lo que se escriba extra
+    textarea.blur(); // Desactiva la edición del textarea
+  }
+});
