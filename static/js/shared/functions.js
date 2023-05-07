26f1d7bee34dd445b2243180c3b7a08b79b664a1
piero <piero.guerrero@utec.edu.pe>
2023-05-07 10:09:21 -0500

    static/js/shared/functions.js: Add change_profile function

--- static/js/shared/functions.js
+++ static/js/shared/functions.js
@@ -20,3 +20,20 @@ function list_games(games) {
         })
     }
 }
+
+function change_profile() {
+    fetch("/profile").then(function (response) {
+        return response.json()
+    }).then(function (user) {
+        const name = document.getElementById("name")
+        name.innerHTML = `Nombre: ${user.firstname} ${user.lastname}`
+
+        const email = document.getElementById("email")
+        email.innerHTML = `Correo electrónico: ${user.email}`
+
+        const bio = document.getElementById("bio")
+        email.innerHTML = `Bio: ${user.bio}`
+
+        list_games(user.games_bought)
+    })
+}
