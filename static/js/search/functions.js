750c7ab4bfbec759b1244f549b7665c0d480136b
piero <piero.guerrero@utec.edu.pe>
2023-05-09 14:11:38 -0500

    static/js/search/: Add functions to dynamically show categories and their elements

--- static/js/search/functions.js
+++ static/js/search/functions.js
@@ -1,3 +1,41 @@
+window.onload = function() {
+    set_information()
+}
+
+function set_information() {
+    update_search("genre", "Todas")
+    update_search("platform", "Todas")
+    update_search("publisher", "Todas")
+    update_search("name", "")
+
+    set_list("genre")
+    set_list("platform")
+    set_list("publisher")
+}
+
+function set_list(categoria) {
+    fetch(`/${categoria}`).then(function (response) {
+        return response.json()
+    }).then(function (elementos) {
+        const list = document.getElementById(`list_${categoria}s`)
+
+        elementos.forEach((elemento) => {
+            const block = document.createElement("li")
+            const classes = block.classList
+            classs.add("sidenav-item")
+            block.id = `${elemento.name}`
+            block.onclick = update_search_params(categoria, elemento.name)
+
+            block.innerHTML = `
+                    <a class="sidenav-link">${elemento.name}</a>
+                    `
+
+            list.appendChild(block)
+        })
+
+    })
+}
+
 function update_search_params(categoria, elemento) {
     let url = new URL(window.location)
     url.searchParams.set(categoria, elemento)
