55923eacb94a6d9265ef20f80b94652fd40c7b96
piero <piero.guerrero@utec.edu.pe>
2023-05-11 13:05:27 -0500

    Move update_search_params from search.js to shared.js

--- static/js/search.js
+++ static/js/search.js
@@ -36,19 +36,6 @@ function set_list(categoria) {
     })
 }
 
-function update_search_params(categoria, elemento) {
-    let url = new URL(window.location)
-    url.searchParams.set(categoria, elemento)
-
-    url.search = url.searchParams
-    url = url.toString()
-
-    history.pushState({}, null, url)
-
-    fetch(`/search`)
-        .then(function (response) {
-            return response.json()
-        }).then(function (games) {
-            list_games(games)
-        })
+window.onload = function() {
+    set_information()
 }
--- static/js/shared.js
+++ static/js/shared.js
@@ -17,3 +17,21 @@ function list_games(games) {
         })
     }
 }
+
+function update_search_params(categoria, elemento) {
+    let url = new URL(window.location)
+    url.searchParams.set(categoria, elemento)
+
+    url.search = url.searchParams
+    url = url.toString()
+
+    history.pushState({}, null, url)
+
+    fetch(`/do_search`)
+        .then(function (response) {
+            return response.json()
+        }).then(function (jsonResponse) {
+            const games = jsonResponse.games
+            list_games(games)
+        })
+}
