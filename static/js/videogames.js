9d2fd2523b787937cd2a11451e10bfc42654a1ec
piero <piero.guerrero@utec.edu.pe>
2023-05-13 15:57:05 -0500

    static/js/videogames.js: Make comprar function check if game has been bought by the user

--- static/js/videogames.js
+++ static/js/videogames.js
@@ -1,24 +1,25 @@
-window.onload = function() {
-    change_videogame()
-}
-
 function change_videogame() {
-    fetch("/videogame").then(function (response) {
+    fetch("/get_videogame").then(function (response) {
         return response.json()
-    }).then(function (game_platform) {
-        const platform_1 = game_platform.platform
+    }).then(function (jsonResponse) {
+        const game_platform = jsonResponse.game_platform
+        const platform_game = game_platform.platform
+
+        const platform = game_platform.platform
         const game_puplisher = game_platform.publisher
         const puplisher = game_publisher.publisher
         const game = game_publisher.game
-        const genre_1 = game.genre
+        const genre_game = game.genre
+
+        const title = document.getElementByTagName("title")[0]
 
         const game_name = document.getElementById("game_name")
 
         game_name.innerHTML = `${game.game_name}`
 
-        const container_image = document.getElementByClassName("image_wide")[0]
+        const container_image = document.getElementById("game_image")
         container_image.innerHTML = `
-                        <img src="static/videogames/${game.image}">
+                        <img src="${game.image}" alt="Game Image">
                         `
 
         const synopsis = document.getElementById("synopsis")
@@ -27,13 +28,48 @@ function change_videogame() {
         const year = document.getElementById("year")
         year.innerHTML = `Año de lanzamiento: ${game_platform.release_year}`
 
-        const genre_2 = document.getElementById("genre")
-        genre.innerHTML = `Sinopsis: ${genre_1.genre_name}`
+        const genre_tag = document.getElementById("genre")
+        genre_tag.innerHTML = `Sinopsis: ${genre_tag.genre_name}`
 
         const publisher = document.getElementById("publisher")
         publisher.innerHTML = `Editor: ${publisher.publisher_name}`
 
-        const platform_2 = document.getElementById("platform")
-        platform.innerHTML = `Plataforma: ${platform_1.platform_name}`
+        const platform_tag = document.getElementById("platform")
+        platform_tag.innerHTML = `Plataforma: ${platform_tag.platform_name}`
     })
+}
+
+function comprar(event) {
+    event.preventDefault();
+
+    fetch("/is_game_bought").then(function (response) {
+        return rsponse.json()
+    }).then(function (jsonResponse) {
+        if (jsonResponse.is_bought == 1) {
+            const buy_message = document.getElementById("buy_message")
+            buy_message.innerHTML = "Ya has comprado este juego"
+        } else {
+            fetch('/buy_game', {
+                method: 'POST',
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success === true) {
+                        window.location.href = '/checkout';
+                    } else {
+                        const buy_message = document.getElementById("buy_message")
+                        buy_message.innerHTML = 'La verificación de compra no fue exitosa'
+                    }
+                })
+                .catch(error => {
+                    const buy_message = document.getElementById("buy_message")
+                    buy_message.innerHTML = 'Error al realizar la verificación de compra:'
+                });
+        }
+    })
+}
+
+window.onload = function() {
+    change_videogame()
+    change_buy_button()
 }
