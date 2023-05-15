681fbc4eb356ed6ddc59e655b0c533343c4ae5aa
piero <piero.guerrero@utec.edu.pe>
2023-05-15 00:41:27 -0500

    static/js/search.js: Fix set_information

--- static/js/search.js
+++ static/js/search.js
@@ -1,20 +1,20 @@
-function set_information(name) {
-    update_search_params("genre", "Todas")
-    update_search_params("platform", "Todas")
-    update_search_params("publisher", "Todas")
+function set_information() {
+    change_search_params("genre", "Todas")
+    change_search_params("platform", "Todas")
+    change_search_params("publisher", "Todas")
 
-    set_list("genre")
-    set_list("platform")
-    set_list("publisher")
+    set_list("genre", "1-0-0")
+    set_list("platform", "1-0-1")
+    set_list("publisher", "1-0-2")
 }
 
-function set_list(categoria) {
+function set_list(categoria, list_id) {
     fetch(`/get_${categoria}`).then(function (response) {
         return response.json()
     }).then(function (jsonResponse) {
         const elementos = jsonResponse.elementos
 
-        const list = document.getElementById(`list_${categoria}s`)
+        const list = document.getElementById(`sidenav-collapse-${list_id}`)
 
         elementos.forEach((elemento) => {
             const block = document.createElement("li")
