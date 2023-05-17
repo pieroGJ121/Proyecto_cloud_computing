b2a09b91d2e84184ec17dfbf09514636d04b5b0c
pieroGJ121 <piero.guerrero@utec.edu.pe>
2023-05-16 19:35:42 -0500

    load.sh: Loop over sql scripts

--- load.sh
+++ load.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env sh
 
-psql -U postgres -d project_dbp -a -f sql/script.sql
-psql -U postgres -d project_dbp -a -f sql/script2.sql
-psql -U postgres -d project_dbp -a -f sql/script3.sql
-psql -U postgres -d project_dbp -a -f sql/script4.sql
-
 python app.py
 
-psql -d project_dbp -a -f sql/script.sql
-psql -d project_dbp -a -f sql/script2.sql
-psql -d project_dbp -a -f sql/script3.sql
-psql -d project_dbp -a -f sql/script4.sql
+for file in script.sql script2.sql script3.sql script4.sql
+do
+    psql -d project_dbp -a -f "sql/$file"
+done
