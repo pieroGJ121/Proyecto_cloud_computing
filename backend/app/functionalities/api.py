8f56476968e288d51c14cba7bd80717e526994e0
pieroGJ121 <piero.guerrero@utec.edu.pe>
2023-07-03 19:41:31 -0500

    backend/app/functionalities/api.py: Fix bug of get_game_api

--- backend/app/functionalities/api.py
+++ backend/app/functionalities/api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
+from datetime import datetime
 
 
 def do_request_api(body, path):
@@ -16,16 +17,16 @@ def do_request_api(body, path):
 
 
 def get_game_info_api(id):
-    fields = "fields name, first_release_year, genres.name, platforms.name, involved_companies.company.name, cover.image_id;"
+    fields = "fields summary, name, first_release_date, genres.name, platforms.name, involved_companies.company.name, cover.image_id;"
     body = fields + " where id = " + id + ";"
     data = do_request_api(body, "games").json()[0]
     return {
         'api_id': id,
         'name': data["name"],
-        'release_year': data["first_release_year"],
-        'genres': data["genres"],
-        'platforms': data["platforms"],
+        'release_year': datetime.utcfromtimestamp(data["first_release_date"]).strftime('%d-%m-%Y'),
+        'genres': [i["name"] for i in data["genres"]],
+        'platforms': [i["name"] for i in data["platforms"]],
         'summary': data["summary"],
-        'involved_companies': data["involved_companies"],
-        'cover': "https:" + data["cover"],
+        'involved_companies': [i["company"]["name"] for i in data["involved_companies"]],
+        'cover': "https:" + data["cover"]["image_id"],
     }
