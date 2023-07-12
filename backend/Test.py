c2524117ffa3f99ef96622373e09cffa35cdd307
pieroGJ121 <piero.guerrero@utec.edu.pe>
2023-07-11 19:28:46 -0500

    backend/Test.py: Add correct user

--- backend/Test.py
+++ backend/Test.py
@@ -17,7 +17,7 @@ class ProyectTests(unittest.TestCase):
             "bio" : "Estudiante de la UTEC",
             "email" : "manuel.silva@utec.edu.pe",
             "password" : "12345678",
-            "confirm_password" : "12345678", 
+            "confirmationPassword" : "12345678",
         }
         self.new_oferta = {
             "game_id" : "dsadsada",
@@ -28,24 +28,43 @@ class ProyectTests(unittest.TestCase):
             "game_id" : None,
         }
 
-    def test_create_usuario_success(self):
-        pass
+        response_user = self.client.post(
+            '/create', json=self.new_usuario)
+        data_user = json.loads(response_user.data)
+        self.user_valid_token = data_user['token']
+        self.user_id = data_user['user_id']
+
+        self.headers = {
+            "content-type": 'application/json'
+        }
+
+    # def test_create_usuario_success(self):
+    #     pass
 
 
-    def test_1_create_oferta_success(self):
-        response = self.client.post('/oferta', json=self.new_oferta )
+    # def test_1_create_oferta_success(self):
+    #     response = self.client.post('/oferta', json=self.new_oferta, headers=self.headers)
+    #     data = json.loads(response.data)
+
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertTrue(data['message'])
+
+    # def test_2_create_oferta_fail(self):
+    #     response = self.client.post('/oferta', json=self.invalid_form, headers=self.headers)
+    #     data = json.loads(response.data)
+
+    #     self.assertEqual(response.status_code, 401)
+    #     self.assertEqual(data['success'], False)
+    #     self.assertTrue(data['message'])
+
+    def test_1_search_tipo(self):
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.get('/search/genres', headers=self.headers)
         data = json.loads(response.data)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertTrue(data['message'])
 
-    def test_2_create_oferta_fail(self):
-        response = self.client.post('/oferta', json=self.invalid_form )
-        data = json.loads(response.data)
-
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(data['success'], False)
-        self.assertTrue(data['message'])
-
-    
+    def tearDown(self):
+        self.client.delete('/usuarios/{}'.format(self.user_id))
