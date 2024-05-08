4ae5c3ee6edcc3745d9ece177dd3cbb1e6afc8b2
pieroGJ121 <piero.guerrero@utec.edu.pe>
2024-05-07 20:30:01 -0500

    backend/app/__init__.py: Add endpoints for review

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -13,6 +13,8 @@ from .models import (
     Game,
     Compra,
     Oferta,
+    Rating,
+    Review,
     setup_db)
 from .functionalities.send_email import enviar_correo
 from flask_migrate import Migrate
@@ -399,7 +401,7 @@ def create_app(test_config=None):
     @authorize
     def obtain_ratings_from_user():
         current_user_id = request.headers["user-id"]
-        ratings = Oferta.query.filter_by(usuario_id=current_user_id).all()
+        ratings = Rating.query.filter_by(usuario_id=current_user_id).all()
         ratings_serialized = [i.get_data_with_game() for i in ratings]
 
         return jsonify({'success': True,
@@ -449,7 +451,7 @@ def create_app(test_config=None):
 
                 db.session.add(rating)
                 db.session.commit()
-                oferta_id = rating.id
+                rating_id = rating.id
         except Exception as e:
             db.session.rollback()
             returned_code = 500
@@ -468,7 +470,7 @@ def create_app(test_config=None):
         else:
             return jsonify({'success': True,
                             'message': 'Rating Created successfully!',
-                            "id": oferta_id}), returned_code
+                            "id": rating_id}), returned_code
 
     @app.route('/rating/<id>', methods=['PATCH'])
     @authorize
@@ -529,6 +531,155 @@ def create_app(test_config=None):
                             'message': 'There is no rating'}), returned_code
         else:
             return jsonify({'success': True, 'message': 'Rating deleted successfully'}), returned_code
+
+
+
+    # Todo referente a review
+
+    @app.route('/review', methods=['GET'])
+    @authorize
+    def obtain_reviews_from_user():
+        current_user_id = request.headers["user-id"]
+        reviews = Review.query.filter_by(usuario_id=current_user_id).all()
+        reviews_serialized = [i.get_data_with_game() for i in reviews]
+
+        return jsonify({'success': True,
+                        'reviews': reviews_serialized}), 200
+
+
+    @app.route('/reviews/<identificador>', methods=['GET'])
+    @authorize
+    def get_reviews(id):
+        current_user_id = request.headers["user-id"]
+        review = Review.query.get(id)
+        if review:
+            if review.usuario_id == current_user_id:
+                review_data = review.get_data_with_game()
+                return jsonify({'success': True,
+                                'review': review_data,
+                                'game': review_data["game"]}), 200
+            else:
+                abort(403)
+        else:
+            abort(404)
+
+    @app.route('/review', methods=['POST'])
+    @authorize
+    def new_review():
+        returned_code = 201
+        list_errors = []
+        review_id = ''
+        try:
+            current_user_id = request.headers["user-id"]
+            body = request.json
+
+            if 'game_id' not in body:
+                list_errors.append('game_id is required')
+            else:
+                game_api_id = body['game_id']
+
+            if 'title' not in body:
+                list_errors.append('title is required')
+            else:
+                title = body['title']
+
+            if 'comment' not in body:
+                list_errors.append('comment is required')
+            else:
+                comment = body['comment']
+
+            if len(list_errors) > 0:
+                returned_code = 400
+            else:
+                review = Review(current_user_id, game_api_id, title, comment)
+
+                db.session.add(review)
+                db.session.commit()
+                review_id = review.id
+        except Exception as e:
+            db.session.rollback()
+            returned_code = 500
+            print(e)
+        finally:
+            db.session.close()
+
+        if returned_code == 400:
+            return jsonify({'success': False,
+                            'message': 'Error creating review',
+                            'errors': list_errors}), returned_code
+        elif returned_code == 500:
+            return jsonify({'success': False,
+                            'message': 'Error!'}), returned_code
+            # abort(returned_code)
+        else:
+            return jsonify({'success': True,
+                            'message': 'Review Created successfully!',
+                            "id": review_id}), returned_code
+
+    @app.route('/review/<id>', methods=['PATCH'])
+    @authorize
+    def update_review(id):
+        returned_code = 200
+        list_errors = []
+        try:
+            review = Review.query.get(id)
+            if not review:
+                returned_code = 404
+            else:
+                body = request.json
+                if 'title' in body:
+                    title = body['title']
+                    review.title = title
+
+                if 'comment' in body:
+                    comment = body['comment']
+                    review.comment = comment
+                db.session.commit()
+
+        except:
+            db.session.rollback()
+            returned_code = 500
+        finally:
+            db.session.close()
+
+        if returned_code == 404:
+            return jsonify({'success': False,
+                            'message': 'There is no review'}), returned_code
+        elif returned_code == 400:
+            return jsonify({'success': False,
+                            'message': 'Error updating review',
+                            'errors': list_errors}), returned_code
+        elif returned_code == 500:
+            return jsonify({'success': False,
+                            'message': 'Error!'}), returned_code
+        else:
+            return jsonify({'success': True,
+                            'message': 'Review updated successfully'}), returned_code
+
+    @app.route('/review/<id>', methods=['DELETE'])
+    @authorize
+    def delete_review(id):
+        returned_code = 200
+        try:
+            review = Review.query.get(id)
+            if not review:
+                returned_code = 404
+            else:
+                db.session.delete(review)
+                db.session.commit()
+
+        except:
+            db.session.rollback()
+            returned_code = 500
+        finally:
+            db.session.close()
+
+        if returned_code == 404:
+            return jsonify({'success': False,
+                            'message': 'There is no review'}), returned_code
+        else:
+            return jsonify({'success': True, 'message': 'Review deleted successfully'}), returned_code
+
 
 
 
