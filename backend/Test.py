033722cabdc52b080a69ba597f1055b054a963b7
pieroGJ121 <piero.guerrero@utec.edu.pe>
2024-05-08 21:55:44 -0500

    backend/Test.py: Add tests for review

--- backend/Test.py
+++ backend/Test.py
@@ -455,6 +455,121 @@ class ProyectTests(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertTrue(data['message'])
 
+    def test_review_get_success(self):
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.get('/review', headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_review_get_id_success(self):
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.post('/review', headers=self.headers,
+                                    json=self.new_review)
+        data = json.loads(response.data)
+        review_id = data["id"]
+
+        response = self.client.get('/review/' + review_id, headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_review_get_id_fail(self):
+        game_id = 1942
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.get('/review'+ str(game_id), headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'])
+
+    def test_review_post_success(self):
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.post('/review', headers=self.headers,
+                                    json=self.new_review)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['message'])
+
+    def test_review_post_fail(self):
+        temp_review = self.new_review
+        temp_review["title"] = "llllllllllllllllllllllllllllllllllllllllll"
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.post('/review', headers=self.headers,
+                                    json=temp_review)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'])
+
+    def test_review_patch_success(self):
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.post('/review', headers=self.headers,
+                                    json=self.new_review)
+        data = json.loads(response.data)
+        review_id = data["id"]
+
+        temp_review = self.new_review
+        temp_review["title"] = "More interesting"
+        response = self.client.patch('/review/' + review_id,
+                                     headers=self.headers,
+                                     json=temp_review)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['message'])
+
+    def test_review_patch_fail(self):
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.post('/review', headers=self.headers,
+                                    json=self.new_review)
+        data = json.loads(response.data)
+        review_id = data["id"]
+
+        temp_review = self.new_review
+        temp_review["title"] = "llllllllllllllllllllllllllllllllllllllllll"
+        response = self.client.patch('/review/' + review_id,
+                                     headers=self.headers,
+                                     json=temp_review)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'])
+
+    def test_review_delete_success(self):
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.post('/review', headers=self.headers,
+                                    json=self.new_review)
+        data = json.loads(response.data)
+        print(data)
+        review_id = data["id"]
+
+        response = self.client.delete('/review/' + review_id,
+                                      headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['message'])
+
+    def test_review_delete_fail(self):
+        self.headers['X-ACCESS-TOKEN'] = self.user_valid_token
+        response = self.client.delete('/review/1', headers=self.headers)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertTrue(data['message'])
+
+
 
 
 
