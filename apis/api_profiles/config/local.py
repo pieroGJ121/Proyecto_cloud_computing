ef189b78e9af755722824430a5dc62cea7e4f192
pieroGJ121 <piero.guerrero@utec.edu.pe>
2024-05-12 13:09:17 -0500

    apis/api_profiles: Reorder directory

---
+++ apis/api_profiles/config/local.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+config = {
+    "DATABASE_URI": "postgresql://postgres:utec@34.228.2.253:8030/api_profiles",
+    "SECRET_KEY": "projectccp2k24",
+    "ALGORYTHM": "HS256",
+}
--- apis/api_profiles/models.py
+++ apis/api_profiles/models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from datetime import datetime
-from .functionalities.api import get_game_info_api
 from config.local import config
 from werkzeug.security import generate_password_hash, check_password_hash
 import sys
@@ -11,31 +10,38 @@ db = SQLAlchemy()
 
 
 def setup_db(app, database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = config['DATABASE_URI'] if database_path is None else database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        config["DATABASE_URI"] if database_path is None else database_path
+    )
     db.app = app
     db.init_app(app)
     db.create_all()
 
 
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'
-    id = db.Column(db.String(36), primary_key=True,
-                   default=lambda: str(uuid.uuid4()),
-                   server_default=db.text("uuid_generate_v4()"))
+    __tablename__ = "usuarios"
+    id = db.Column(
+        db.String(36),
+        primary_key=True,
+        default=lambda: str(uuid.uuid4()),
+        server_default=db.text("uuid_generate_v4()"),
+    )
     firstname = db.Column(db.String(80), nullable=False)
     lastname = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(300), unique=True, nullable=False)
     password_hash = db.Column(db.String(400), nullable=False)
     bio = db.Column(db.Text, nullable=False)
 
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False,
-                           server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable=True,
-                            server_default=db.text("now()"))
+    created_at = db.Column(
+        db.DateTime(timezone=True), nullable=False, server_default=db.text("now()")
+    )
+    modified_at = db.Column(
+        db.DateTime(timezone=True), nullable=True, server_default=db.text("now()")
+    )
 
     @property
     def password(self):
-        raise AttributeError('Password is not readable')
+        raise AttributeError("Password is not readable")
 
     @password.setter
     def password(self, password):
@@ -57,7 +63,7 @@ class Usuario(db.Model):
         self.created_at = datetime.utcnow()
 
     def __repr__(self):
-        return '<Usuario %r %r>' % (self.firstname, self.lastname)
+        return "<Usuario %r %r>" % (self.firstname, self.lastname)
 
     def insert(self):
         try:
@@ -66,7 +72,7 @@ class Usuario(db.Model):
             user_created_id = self.id
         except Exception as e:
             print(sys.exc_info())
-            print('e: ', e)
+            print("e: ", e)
             db.session.rollback()
         finally:
             db.session.close()
@@ -79,16 +85,16 @@ class Usuario(db.Model):
             db.session.commit()
         except Exception as e:
             print(sys.exc_info())
-            print('e: ', e)
+            print("e: ", e)
             db.session.rollback()
 
     def serialize(self):
         return {
-            'id': self.id,
-            'name': self.firstname,
-            'lastname': self.lastname,
-            'email': self.email,
-            'bio': self.bio,
-            'created_at': self.created_at,
-            'modified_at': self.modified_at,
+            "id": self.id,
+            "name": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+            "bio": self.bio,
+            "created_at": self.created_at,
+            "modified_at": self.modified_at,
         }
